c8a9ba5fd42de644a61ad73a3ba3dc430ccb9ef8
zhaohq <zhaohq@bpgroup.com.cn>
2023-08-07 18:09:22 +0800

    feat: logback 日志配置

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Springboot3.0-HelloApp
-SpringBoot3.0项目，测试集成Mybatis-plus,Nacos,ShardingSphere
+SpringBoot3.0测试项目，测试集成Mybatis-plus,Nacos,ShardingSphere
+
+## Logback引入 分支:feature-logback
---
+++ src/main/java/com/hello/springboot/HelloSpringBoot3XApplicationRunner.java
@@ -0,0 +1,14 @@
+package com.hello.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class HelloSpringBoot3XApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBoot3XApplicationRunner.class, args);
+        log.info("服务器启动成功");
+    }
+}
