586b78f54c325748c8cf1b4649ae49b060cedfd4
zhaohq <zhaohq@bpgroup.com.cn>
2023-08-08 17:07:51 +0800

    feat: 主从同步

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ SpringBoot3.0测试项目，测试集成Mybatis-plus,Nacos,ShardingSphere
 
 
 ## Mybatis Mybatis-plus 引入 分支: feature-mybatisplus
+
+
+## ShardingSphere引入实现读写分离, 未测试数据分片 分支: feature-shardingsphere
--- src/test/java/com/hello/springboot/SystemUserMybatisPlusTest.java
+++ src/test/java/com/hello/springboot/SystemUserMybatisPlusTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 @Slf4j
 @ActiveProfiles({"dev", "personal"})
 @SpringBootTest(classes = HelloSpringBoot3XApplicationRunner.class)
@@ -23,5 +25,23 @@ public class SystemUserMybatisPlusTest {
     void test() {
         SystemUser systemUser = systemUserService.getById(1);
         log.info("查询到的用户:{}", systemUser);
+    }
+
+    @Test
+    void test2() {
+        List<SystemUser> users = systemUserService.list();
+        for (SystemUser user : users) {
+            log.info("查询到的用户:{}", user);
+        }
+
+        SystemUser user = new SystemUser();
+        user.setUserName("测试用户");
+        boolean save = systemUserService.save(user);
+        log.info("插入用户成功: {}", save);
+        // 再读一次，数据库没开主从同步，执行完检查下主库是否写入数据
+        List<SystemUser> users2 = systemUserService.list();
+        for (SystemUser user2 : users2) {
+            log.info("查询到的用户:{}", user2);
+        }
     }
 }
